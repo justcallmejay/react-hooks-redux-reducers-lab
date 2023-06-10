e0419fb63596340d1c405d6f48e83b4f7b1e4786
jay <jaypifiverr@gmail.com>
2023-06-10 04:55:56 -0400

    commit tree e1016bff6617ad78e03a095b97dec2bee8bf656a

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,9 @@
 export function manageFriends(state, action) {
   // your code here
+switch (action.type) {
+  case
 }
+  
+}
+
+let state = { friends: 0 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,15 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+    } 
 }
+
+// function managePresents(state, action)
+
+// state = { numberOfPresents : 0 }
+// action = { type : "presents/increase" }
